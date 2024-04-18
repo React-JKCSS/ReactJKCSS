ded55089a14de31658596938223489456c05a982
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-18 20:46:03 +0530

    Update ButtonYellow.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Bottons/ButtonYellow/ButtonYellow.jsx
+++ src/Bottons/ButtonYellow/ButtonYellow.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '../app.css'
+import '../../Styles/app.css'
 
 const ButtonYellow = ({ onClick, children }) => (
     <button className="jkbtn jkbtn-yellow" onClick={onClick}>
