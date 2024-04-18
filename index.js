7a815a39cf3280417d73f497e3607440239a3bb5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-18 20:50:30 +0530

    Update index.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ export { default as ButtonBlue } from './src/Bottons/ButtonBlue/ButtonBlue';
 
 export { default as ButtonGray } from './src/Bottons/ButtonGray/ButtonGray';
 export { default as ButtonGreen } from './src/Bottons/ButtonGreen/ButtonGreen';
-export { default as ButtonRed } from './src/Bottons/ButtonRed/ButtonRed';
+export { default as ButtonInfo } from './src/Bottons/ButtonInfo/ButtonInfo';
 export { default as ButtonRed } from './src/Bottons/ButtonRed/ButtonRed';
 export { default as ButtonRed } from './src/Bottons/ButtonRed/ButtonRed';
