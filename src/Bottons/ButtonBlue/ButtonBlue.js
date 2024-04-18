d4ea58ce4ce0eb6ee75c2dd69eee99ac1b3eb499
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-18 20:00:49 +0530

    Update ButtonBlue.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Bottons/ButtonBlue/ButtonBlue.js
+++ src/Bottons/ButtonBlue/ButtonBlue.js
@@ -1,2 +1,10 @@
 import React from "react";
 import '../Buttons.css'
+
+const ButtonBlue = ({ onClick, children }) => (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+  
+  export default Button;
