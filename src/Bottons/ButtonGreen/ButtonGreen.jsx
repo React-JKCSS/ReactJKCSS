cf35c891477644316ffb9a6513617c660903449e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-18 20:44:42 +0530

    Update ButtonGreen.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Bottons/ButtonGreen/ButtonGreen.jsx
+++ src/Bottons/ButtonGreen/ButtonGreen.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '../app.css'
+import '../../Styles/app.css'
 
 const ButtonGreen = ({ onClick, children }) => (
     <button className="jkbtn jkbtn-green" onClick={onClick}>
