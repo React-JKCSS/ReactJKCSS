3cd46fbf0691f56fb431dc54eca7b99d53148151
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-16 06:04:47 +0530

    Create BasicTableHover.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ src/Tables/BasicTable/BasicTableHover.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import '../../Styles/app.css'
+import PropTypes from 'prop-types';
+
+const BasicTableHover = ({ columns, data }) => (
+    <table className="jk-table">
+        <thead>
+            <tr>
+                {
+                    columns.map((column, index) => (
+                        <th key={index}>{column}</th>
+                    ))
+                }
+            </tr>
+        </thead>
+        <tbody>
+            {
+                data.map((row, rowIndex) => (
+                    <tr key={rowIndex}>
+                        {columns.map((column, colIndex) => (
+                            <td key={colIndex}>{row[column]}</td>
+                        ))}
+                    </tr>
+                ))
+            }
+        </tbody>
+  </table>
+);
+
+// check props are not empty
+
+// BasicTable.propTypes = {
+//     columns: PropTypes.arrayOf(PropTypes.string).isRequired,
+//     data: PropTypes.arrayOf(PropTypes.object).isRequired
+// };
+
+
+export default BasicTableHover;
