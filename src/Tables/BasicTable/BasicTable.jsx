c12c422c4b84bc5716d916fb791ddba755e7ea74
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-16 05:37:23 +0530

    Update BasicTable.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Tables/BasicTable/BasicTable.jsx
+++ src/Tables/BasicTable/BasicTable.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import '../../Styles/app.css'
 
 const BasicTable = ({ columns, data }) => (
-    
+    <table>
+        
+    </table>
 );
+
+// check props are not empty
+
+BasicTable.propTypes = {
+    columns: PropTypes.arrayOf(PropTypes.string).isRequired,
+    data: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
 
 export default BasicTable;
