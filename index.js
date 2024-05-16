535bc3a04499728b5e402192068bed0bc04fdbef
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-16 06:11:45 +0530

    Update index.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ export { default as ButtonInfo } from './src/Bottons/ButtonInfo/ButtonInfo';
 export { default as ButtonRed } from './src/Bottons/ButtonRed/ButtonRed';
 export { default as ButtonYellow } from './src/Bottons/ButtonYellow/ButtonYellow';
 
+// ButtonCustom
+export { default as ButtonCustom} from './src/Bottons/ButtonCustom/ButtonCustom';
+
 // for tables
 export { default as BasicTable } from './src/Tables/BasicTable/BasicTable';
 export { default as BasicTableHover } from './src/Tables/BasicTable/BasicTableHover';
