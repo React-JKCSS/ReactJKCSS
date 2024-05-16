53f804c5f8e7ca560d0f41dcd30863e4f8da78a4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-16 05:38:36 +0530

    Update BasicTable.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Tables/BasicTable/BasicTable.jsx
+++ src/Tables/BasicTable/BasicTable.jsx
@@ -3,8 +3,27 @@ import '../../Styles/app.css'
 
 const BasicTable = ({ columns, data }) => (
     <table>
-        
-    </table>
+        <thead>
+            <tr>
+                {
+                    columns.map((column, index) => (
+                        <th key={index}>{column}</th>
+                    ))
+                }
+            </tr>
+        </thead>
+        <tbody>
+            {
+                data.map((row, rowIndex) => (
+                    <tr key={rowIndex}>
+                        {columns.map((column, colIndex) => (
+                            <td key={colIndex}>{row[column]}</td>
+                        ))}
+                    </tr>
+                ))
+            }
+        </tbody>
+  </table>
 );
 
 // check props are not empty
