e1ab3fb8d993f5b0602e1df278251ab7086557b6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-01 20:47:10 +0530

    Create ButtonLargeYellow.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ src/Bottons/ButtonYellow/ButtonLargeYellow.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../Styles/app.css'
+
+const ButtonLargeYellow = ({ onClick, children }) => {
+  return (
+    <div>ButtonLargeYellow</div>
+  )
+}
+
+export default ButtonLargeYellow
