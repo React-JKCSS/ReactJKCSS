969d26c2031e8d5c4280dd78eadf7da4ce6f82b7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-11-05 10:54:44 +0530

    Update

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-export { default as ButtonBlue } from './src/Bottons/ButtonBlue/ButtonBlue';
-export { default as ButtonGray } from './src/Bottons/ButtonGray/ButtonGray';
-export { default as ButtonGreen } from './src/Bottons/ButtonGreen/ButtonGreen';
-export { default as ButtonInfo } from './src/Bottons/ButtonInfo/ButtonInfo';
-export { default as ButtonRed } from './src/Bottons/ButtonRed/ButtonRed';
-export { default as ButtonYellow } from './src/Bottons/ButtonYellow/ButtonYellow';
-
-export { default as ButtonLargeBlue } from './src/Bottons/ButtonBlue/ButtonLargeBlue';
-export { default as ButtonSmallBlue } from './src/Bottons/ButtonBlue/ButtonSmallBlue';
-export { default as ButtonLargeGray } from './src/Bottons/ButtonGray/ButtonLargeGray';
-export { default as ButtonSmallGray } from './src/Bottons/ButtonGray/ButtonSmallGray';
-export { default as ButtonLargeGreen } from './src/Bottons/ButtonGreen/ButtonLargeGreen'; 
-export { default as ButtonSmallGreen } from './src/Bottons/ButtonGreen/ButtonSmallGreen';
-export { default as ButtonLargeInfo } from './src/Bottons/ButtonInfo/ButtonLargeInfo';
-export { default as ButtonSmallInfo } from './src/Bottons/ButtonInfo/ButtonSmallInfo';
-export { default as ButtonLargeRed } from './src/Bottons/ButtonRed/ButtonLargeRed';
-export { default as ButtonSmallRed } from './src/Bottons/ButtonRed/ButtonSmallRed';
-export { default as ButtonLargeYellow } from './src/Bottons/ButtonYellow/ButtonLargeYellow';
-export { default as ButtonSmallYellow } from './src/Bottons/ButtonYellow/ButtonSmallYellow';
-
 // Default btn
 
-export { default as DefaultBtn } from './src/Bottons/DefaultBtn/DefaultBtn'
+export { default as Button } from './src/Bottons/DefaultBtn/DefaultBtn'
 
 // ButtonCustom
 export { default as ButtonCustom } from './src/Bottons/ButtonCustom/ButtonCustom';
--- src/Bottons/DefaultBtn/DefaultBtn.jsx
+++ src/Bottons/DefaultBtn/DefaultBtn.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import '../../Styles/testStyle.css'
+import React from 'react';
+import '../../Styles/testStyle.css';
 
-const DefaultBtn = ({ style, children, onClick }) => {
-  return (
-    <button className={style} onClick={onClick}>
-        {children}
-    </button>
-  )
-}
+const Button = ({ Btntype, style, children, onClick }) => {
+    let buttonStyle = '';
+    const Colors = ['blue', 'gray', 'green', 'red', 'yellow', 'info']
+    let customStyles = {};
 
-export default DefaultBtn
+    if (!Colors.includes(style)) {
+        customStyles = {
+            backgroundColor: style,
+            color: 'white',
+        };
+        buttonStyle = 'jkbtn';
+    }
+
+    else {
+        buttonStyle = !Btntype ? `jkbtn jkbtn-${style}` : `jkbtn-${Btntype} jkbtn-${style}`;
+    }
+
+
+    return (
+        <button className={buttonStyle} style={customStyles} onClick={onClick} >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
