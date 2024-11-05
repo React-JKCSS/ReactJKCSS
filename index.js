f510934f41af2aa8850c272000816f35221a93b5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-11-05 11:13:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,6 @@
 
 export { default as Button } from './src/Bottons/DefaultBtn/DefaultBtn'
 
-// ButtonCustom
-export { default as ButtonCustom } from './src/Bottons/ButtonCustom/ButtonCustom';
-export { default as ButtonCustomLarge } from './src/Bottons/ButtonCustom/ButtonCustomLarge';
-export { default as ButtonCustomSmall } from './src/Bottons/ButtonCustom/ButtonCustomSmall';
 
 // for tables
 export { default as BasicTable } from './src/Tables/BasicTable/BasicTable';
